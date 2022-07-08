f985ec77e1586d9a5d0c43227cf6f3f34846a2b0
iamraufu <raufu.dev@gmail.com>
2022-07-08 21:26:12 +0600

    [ADD] Firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6icArRAnZV7J-Htbt7x0KRlN8JpK2OpQ",
+  authDomain: "raufuautomotive.firebaseapp.com",
+  projectId: "raufuautomotive",
+  storageBucket: "raufuautomotive.appspot.com",
+  messagingSenderId: "958474828891",
+  appId: "1:958474828891:web:7b4d8b84d322c9341c137e"
+};
+
+export default firebaseConfig;
