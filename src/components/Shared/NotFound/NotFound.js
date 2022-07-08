c078e66f721fab70fac679c336fb9bc24624129e
iamraufu <raufu.dev@gmail.com>
2022-07-08 21:23:31 +0600

    [UPDATED] NotFound

--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './NotFound.css';
 import Navbar from '../Navbar/Navbar';
 
 const NotFound = () => {
     return (
-        <div className="bg-brand bg-brand-container">
+        <div className="bg-brand bg-brand-container not-found-container">
             <Navbar />
-            <h1 className='fw-bold text-center text-danger mt-5 pt-5'>404 <br />Not Found</h1>
+            {/* <h1 className='fw-bold text-center text-danger mt-5 pt-5'>404 <br />Not Found</h1> */}
         </div>
     );
 };
