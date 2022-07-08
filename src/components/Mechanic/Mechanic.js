28c48157cf90fe64360a286ec5e731b4a57a3225
iamraufu <raufu.dev@gmail.com>
2022-07-08 21:21:09 +0600

    [UPDATED] Mechanic

--- src/components/Mechanic/Mechanic.js
+++ src/components/Mechanic/Mechanic.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
 import { useParams, useNavigate } from 'react-router-dom';
-import mechanics from '../../data/mechanicData.json';
+// import mechanics from '../../data/mechanicData.json';
+import { addToBookings } from '../../utilities/bookingUtilities';
+import logo from '../../images/logo.svg';
 
 const Mechanic = () => {
+    
+    const { id } = useParams();
     // eslint-disable-next-line
     const navigate = useNavigate();
 
-    
-    const { id } = useParams();
-    console.log(id)
-    const mechanic = mechanics.find(mechanic => mechanic.id === id);
+    const [mechanic, setMechanic] = useState({});
+
+    useEffect(()=>{
+        fetch(`http://localhost:8000/mechanic/${id}`)
+        .then(res => res.json())
+        .then(data => setMechanic(data))
+    }
+    ,[id])
 
     return (
         <div className='bg-brand bg-brand-container'>
@@ -18,25 +26,29 @@ const Mechanic = () => {
             <div className="container">
                 <h1 className='mt-5 fs-4 text-center'>Product Details</h1>
 
-                <div className="row mt-5 justify-content-center align-items-center">
+                {
+                    mechanic?._id ? <div className="row mt-5 justify-content-center align-items-center">
 
                     <div className="col-lg-4">
                         <img src={mechanic.image} style={{ borderRadius: '1rem', boxShadow: '0 5px 15px #c4c4c44d' }} className='img-fluid mx-auto d-block mb-3 mechanic-image' width={250} alt={mechanic.image} />
                         <div className="d-flex justify-content-center align-items-center">
-                            <button 
-                            // onClick={() => addToCart(mechanic)} 
+                            <button onClick={() => addToBookings(mechanic._id)} 
                             className='btn btn-dark mt-2 fw-bold'>Book Now</button>
                         </div>
                     </div>
 
                     <div className="col-lg-8 mb-3">
-                        <div style={{ borderRadius: '1rem', boxShadow: '0 5px 15px #c4c4c44d' }} className="bg-white p-5 mx-auto">
+                        <div style={{ borderRadius: '1rem', boxShadow: '0 5px 15px #c4c4c44d' }} className="bg-white p-5 mx-auto mt-3">
                             <h3 className='fs-5 fw-bold'>{mechanic.name}</h3>
                             <hr />
                             <p style={{ textAlign: 'justify' }} className='fs-6'>{mechanic.description}</p>
                         </div>
                     </div>
-                </div>
+                </div> : <div style={{ position: 'absolute', height: '100px', width: '100px', top: '50%', left: '50%', marginLeft: '-50px', marginTop: '-50px' }}>
+                        <img src={logo} id='breathing' width={100} height={100} className='img-fluid' alt="logo of Raufu Automotive" />
+                        <p className='text-center'>Loading...</p>
+                      </div>
+                }
             </div>
         </div>
     );
