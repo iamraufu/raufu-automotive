2e81735ed0d111e4d734605988a20990ec8d7cca
iamraufu <raufu.dev@gmail.com>
2022-07-08 21:22:02 +0600

    [UPDATED] Navbar

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from '../../../images/logo.svg';
 import { NavLink } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const Navbar = () => {
+
+    const { user } = useAuth();
+
     return (
         <nav style={{ backgroundColor: '#f3f5f9', boxShadow: '0 5px 15px #c4c4c44d' }} className="navbar navbar-expand-lg customize-navbar sticky-top">
             <div className="container">
 
                 <NavLink className="navbar-brand" to="/">
-                    <img src={logo} width={50} className='img-fluid' alt="logo" /> Raufu Automotive
+                    <img src={logo} width={50} style={{transform: 'scaleX(-1)'}} className='img-fluid' alt="logo" /> Raufu Automotive
                 </NavLink>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,13 +34,16 @@ const Navbar = () => {
                             };
                         }} className="nav-link" to="/mechanics">Mechanics</NavLink>
 
-                        <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
-                            return {
-                                color: isActive ? "red" : "",
-                            };
-                        }} className="nav-link" to="/bookings">Bookings</NavLink>
+                        {
+                            user.email && 
+                            <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
+                                return {
+                                    color: isActive ? "red" : "",
+                                };
+                            }} className="nav-link" to="/bookings">Bookings</NavLink>
+                        }
 
-                        {/* {
+                        {
                             !user.email && <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                                 return {
                                     color: isActive ? "red" : "",
@@ -50,15 +57,16 @@ const Navbar = () => {
                                     color: isActive ? "red" : "",
                                 };
                             }} className="nav-link" to="/profile">Profile</NavLink>
-                        } */}
+                        } 
+                       
 
-                        {/* {
-                            user.email && <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
+                        {
+                            (user.email === 'eftykharrahman@gmail.com') && <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                                 return {
                                     color: isActive ? "red" : "",
                                 };
                             }} className="nav-link" to="/dashboard">Dashboard</NavLink>
-                        } */}
+                        }
 
                     </div>
 
