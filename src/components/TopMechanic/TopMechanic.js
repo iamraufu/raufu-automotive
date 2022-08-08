46426d45c5e27924053d23f15c3cd5b5d88b3264
iamraufu <raufu.dev@gmail.com>
2022-08-09 05:22:08 +0600

    [UPDATED] TopMechanic

--- src/components/TopMechanic/TopMechanic.js
+++ src/components/TopMechanic/TopMechanic.js
@@ -13,7 +13,7 @@ const TopMechanic = () => {
     const [topMechanics, setTopMechanics] = useState([]);
 
     useEffect(()=>{
-        fetch('https://raufuautomotive.herokuapp.com/mechanics')
+        fetch('http://raufuautomotive.herokuapp.com/mechanics')
         .then(res => res.json())
         .then(data => setMechanics(data))
     },[])
